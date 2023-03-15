158a92f33982d9386ae7a3fa899adc7792a9052b
j0srisk <j0srisk@gmail.com>
2023-03-15 11:02:31 -0500

    Added Content Swtiching Functionality

--- src/index.js
+++ src/index.js
@@ -2,15 +2,37 @@ import './styles.css';
 import loadNavbar from './modules/navbar';
 import loadHome from './modules/home';
 import loadMenu from './modules/menu';
+import loadContact from './modules/contact';
 import loadFooter from './modules/footer';
 
 const content = document.getElementById('content');
 
-function render() {
-  content.innerHTML = '';
-  content.appendChild(loadNavbar());
-  content.appendChild(loadHome());
-  content.appendChild(loadFooter());
+function render(page) {
+    content.innerHTML = '';
+    let navbar = loadNavbar();
+    content.appendChild(navbar);
+    const homeLink = navbar.querySelector('#home');
+    const menuLink = navbar.querySelector('#menu');
+    const contactLink = navbar.querySelector('#contact');
+    homeLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log('Home link clicked');
+        render(loadHome());
+    });
+
+    menuLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log('Menu link clicked');
+        render(loadMenu());
+    });
+
+    contactLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log('Contact link clicked');
+        render(loadContact());
+    });
+    content.appendChild(page);
+    content.appendChild(loadFooter());
 }
 
-render();
+render(loadHome());
